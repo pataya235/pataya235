c4c50362c6589602ba9faac553b3620a7f147bf1
pataya235 <57751351+pataya235@users.noreply.github.com>
2019-11-14 10:56:41 +0100

    Add files via upload

---
+++ Hello Vault/src/Hello Vault.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello.cpp
+// Author      : Felix Aßmann
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Will this be saved in Arctic Code Vault on 2.2.2020?" << endl;
+	return 0;
+}
